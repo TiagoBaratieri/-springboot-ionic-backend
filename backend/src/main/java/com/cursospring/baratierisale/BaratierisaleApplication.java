095c89e681b80d004eb9001d7663b49e26286eda
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-01-22 14:44:25 -0300

    Project created

---
+++ backend/src/main/java/com/cursospring/baratierisale/BaratierisaleApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.baratierisale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaratierisaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaratierisaleApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/cursospring/baratierisale/BaratierisaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.baratierisale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaratierisaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
