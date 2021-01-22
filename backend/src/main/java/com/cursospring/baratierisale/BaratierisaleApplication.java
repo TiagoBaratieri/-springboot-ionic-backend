e295548eac0178fa9b330605f1cc9e147638da3a
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-01-22 16:43:16 -0300

    Creating instantiation operation

--- backend/src/main/java/com/cursospring/baratierisale/BaratierisaleApplication.java
+++ backend/src/main/java/com/cursospring/baratierisale/BaratierisaleApplication.java
@@ -1,13 +1,33 @@
 package com.cursospring.baratierisale;
 
+import com.cursospring.baratierisale.entities.Category;
+import com.cursospring.baratierisale.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BaratierisaleApplication {
+public class BaratierisaleApplication  implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(BaratierisaleApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+
+    }
 }
+
